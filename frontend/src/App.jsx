2b8a8ebe0294de3ad385bafe2c08e8a47723da92
Veera Sankar <sankarsivam6991@gmail.com>
2025-04-18 11:10:56 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-green-100 min-h-screen flex items-center justify-center">
+      <h1>Welcome to the React App</h1>
+    </div>
+  );
+};
+
+export default App;
